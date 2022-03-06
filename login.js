58dbfb38aa9925c6baadaea3f3a9e4236857e7e4
vani <vanisbala@gmail.com>
2022-03-05 20:25:48 -0800

    changed the email entry

--- login.js
+++ login.js
@@ -53,7 +53,7 @@
         .auth()
         .signInWithPopup(provider)
         .then(function (result) {
-          loggedInStatus.innerText = `You are logged in using the following email: ${result.user.email}`;
+          loggedInStatus.innerText = `You are logged in using the following email: ${result.email.value}`;
           login.style.display = "none";
           signup.style.display = "none";
           email.style.display = "none";
